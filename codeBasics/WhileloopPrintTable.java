bce0c5fc56b0de1ac2ef120d22e0f2482ebcb21d
azees-abdul <azeesabdul2025@gmail.com>
2024-12-27 01:05:56 -0800

    Create WhileloopPrintTable.java

---
+++ codeBasics/WhileloopPrintTable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i =10;
+       while(i!=0){
+           System.out.print(n*i+ " ");
+           i--;
+       }
+    }
+}
