feb39c4bfc45f6bd9e86d5f455c5e58b7ddfd95e
azees-abdul <azeesabdul2025@gmail.com>
2024-12-27 01:24:05 -0800

    Create passbyRef.java

---
+++ codeBasics/passbyRef.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        return new int[]{a+1,b+2};
+        
+    }
+}
+
+
+//Pyhton 
+class SOlution:  
+ def Solution(m,n):
+  return [m+1,n+2]
